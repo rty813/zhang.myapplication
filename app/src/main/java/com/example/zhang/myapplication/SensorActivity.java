f4af2a70af48236b80e2d7391724a622a356395d
rty813 <523213189@qq.com>
2016-11-17 09:15:46 +0800

    加入了环境传感器

--- app/src/main/java/com/example/zhang/myapplication/SensorActivity.java
+++ app/src/main/java/com/example/zhang/myapplication/SensorActivity.java
@@ -12,10 +12,26 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.TextView;
 
+import static android.hardware.Sensor.TYPE_AMBIENT_TEMPERATURE;
+import static android.hardware.Sensor.TYPE_LIGHT;
+import static android.hardware.Sensor.TYPE_PRESSURE;
 import static android.hardware.Sensor.TYPE_PROXIMITY;
+import static android.hardware.Sensor.TYPE_TEMPERATURE;
+import static android.hardware.SensorManager.LIGHT_CLOUDY;
+import static android.hardware.SensorManager.LIGHT_FULLMOON;
+import static android.hardware.SensorManager.LIGHT_NO_MOON;
+import static android.hardware.SensorManager.LIGHT_OVERCAST;
+import static android.hardware.SensorManager.LIGHT_SHADE;
+import static android.hardware.SensorManager.LIGHT_SUNLIGHT;
+import static android.hardware.SensorManager.LIGHT_SUNLIGHT_MAX;
+import static android.hardware.SensorManager.LIGHT_SUNRISE;
 
 public class SensorActivity extends AppCompatActivity implements SensorEventListener {
-    private TextView tv1,tv2,tv3;
+    private TextView tv1;
+    private TextView tv2;
+    private TextView tv3;
+    private TextView lightTv;
+    private TextView pressureTv;
     private SensorManager mSensorManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +52,8 @@ public class SensorActivity extends AppCompatActivity implements SensorEventList
         tv1 = (TextView) findViewById(R.id.Sensor_Proximity_textview);
         tv2 = (TextView) findViewById(R.id.Sensor_Accelerometer_textview);
         tv3 = (TextView) findViewById(R.id.Sensor_Gravity_TextView);
+        lightTv = (TextView) findViewById(R.id.Sensor_Light_Textview);
+        pressureTv = (TextView) findViewById(R.id.Sensor_Pressure_Textview);
         mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
     }
 
@@ -50,13 +68,19 @@ public class SensorActivity extends AppCompatActivity implements SensorEventList
         super.onResume();
         mSensorManager.registerListener(this,
                 mSensorManager.getDefaultSensor(TYPE_PROXIMITY),
-                SensorManager.SENSOR_DELAY_NORMAL);
+                SensorManager.SENSOR_DELAY_FASTEST);
         mSensorManager.registerListener(this,
                 mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
-                SensorManager.SENSOR_DELAY_NORMAL);
+                SensorManager.SENSOR_DELAY_FASTEST);
         mSensorManager.registerListener(this,
                 mSensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY),
-                SensorManager.SENSOR_DELAY_NORMAL);
+                SensorManager.SENSOR_DELAY_FASTEST);
+        mSensorManager.registerListener(this,
+                mSensorManager.getDefaultSensor(TYPE_LIGHT),
+                SensorManager.SENSOR_DELAY_FASTEST);
+        mSensorManager.registerListener(this,
+                mSensorManager.getDefaultSensor(TYPE_PRESSURE),
+                SensorManager.SENSOR_DELAY_FASTEST);
     }
 
     @Override
@@ -64,21 +88,44 @@ public class SensorActivity extends AppCompatActivity implements SensorEventList
         switch(sensorEvent.sensor.getType())
         {
             case Sensor.TYPE_PROXIMITY:
-                String distance = "距离："+sensorEvent.values[0];
-                tv1.setText(distance);
+                if (sensorEvent.values[0] > 1e-5)
+                    tv1.setText("无遮挡");
+                else
+                    tv1.setText("有遮挡");
                 break;
             case Sensor.TYPE_ACCELEROMETER:
-                String accelerometer = "加速度：\n"+"X:"+sensorEvent.values[0]
-                        +"\nY:"+sensorEvent.values[1]
-                        +"\nZ:"+sensorEvent.values[2];
+                String accelerometer = String.format("加速度:\nX:%.2f\nY:%.2f\nZ:%.2f",
+                        sensorEvent.values[0],sensorEvent.values[1],sensorEvent.values[2]);
                 tv2.setText(accelerometer);
                 break;
             case Sensor.TYPE_GRAVITY:
-                String Gravity = "重力：\n"+"X:"+sensorEvent.values[0]
-                        +"\nY:"+sensorEvent.values[1]
-                        +"\nZ:"+sensorEvent.values[2];
+                String Gravity = String.format("重力:\nX:%.2f\nY:%.2f\nZ:%.2f",
+                        sensorEvent.values[0],sensorEvent.values[1],sensorEvent.values[2]);
                 tv3.setText(Gravity);
                 break;
+            case Sensor.TYPE_PRESSURE:
+                pressureTv.setText("压强："+sensorEvent.values[0]+"Pa");
+                break;
+            case Sensor.TYPE_LIGHT:
+                String light = "";
+                light = "光照强度："+sensorEvent.values[0]+"lx\n";
+                if (sensorEvent.values[0] >= LIGHT_SUNLIGHT_MAX)
+                    light += "沙漠地带";
+                else if (sensorEvent.values[0] >= LIGHT_SUNLIGHT)
+                    light += "万里无云阳光直射";
+                else if (sensorEvent.values[0] >= LIGHT_SHADE)
+                    light += "有阳光，有云彩";
+                else if (sensorEvent.values[0] >= LIGHT_OVERCAST)
+                    light += "多云";
+                else if (sensorEvent.values[0] >= LIGHT_SUNRISE)
+                    light += "日出";
+                else if (sensorEvent.values[0] >= LIGHT_CLOUDY)
+                    light += "阴雨，没有太阳 >_<";
+                else if (sensorEvent.values[0] >= LIGHT_FULLMOON)
+                    light += "夜晚，有月光or路灯";
+                else if (sensorEvent.values[0] >= LIGHT_NO_MOON)
+                    light += "漆黑一片";
+                lightTv.setText(light);
         }
     }
 
